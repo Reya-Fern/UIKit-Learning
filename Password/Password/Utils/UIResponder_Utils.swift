b6b38b2e9aa8993983ee976dc17d73672ef1c459
Wannipa Reya <wannipa@siamsquared.com>
2025-08-16 20:42:20 +0700

    Handle view adjustments for keyboard

---
+++ Password/Password/Utils/UIResponder_Utils.swift
@@ -0,0 +1,27 @@
+//
+//  UIResponder_Utils.swift
+//  Password
+//
+//  Created by Wannipa Reya on 16/8/2568 BE.
+//
+
+import UIKit
+
+extension UIResponder {
+
+    private struct Static {
+        static weak var responder: UIResponder?
+    }
+
+    /// Finds the current first responder
+    /// - Returns: the current UIResponder if it exists
+    static func currentFirst() -> UIResponder? {
+        Static.responder = nil
+        UIApplication.shared.sendAction(#selector(UIResponder._trap), to: nil, from: nil, for: nil)
+        return Static.responder
+    }
+
+    @objc private func _trap() {
+        Static.responder = self
+    }
+}
